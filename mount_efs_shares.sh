b2fb1a10ace4577e8fd19ccd886fef5ad014e695
fildens <fildens@mail.ru>
2023-03-15 22:16:53 +0300

    fix mount shares

--- mount_efs_shares.sh
+++ mount_efs_shares.sh
@@ -6,7 +6,9 @@ ms=$1
 DIR="${BASH_SOURCE%/*}"
 export $(grep -v '^#' "${DIR}/.env" | xargs)
 
-mkdir -p /mnt/"$ms"
-/usr/bin/efs-client --host=$ES_HOST --user=$ES_USER --password=$ES_PASS --subfolder=/Unmanaged/"$ms"_1/Content --volume-name=ARCHIVE /mnt/"$ms"
+mkdir -p /mnt/${ms}
+
+# /usr/bin/efs-client --host=${ES_HOST} --user=${ES_USER} --password=${ES_PASS} --subfolder=/Unmanaged/${ms}_1/Content --volume-name=${ms} /mnt/${ms}
+/usr/bin/efs-client --host=10.2.0.1 --user=robot --password=robot123 --subfolder=/Unmanaged/${ms}_1/Content --volume-name=${ms} /mnt/${ms}
 
 exit 0
--- mount_raidix_arc.sh
+++ mount_raidix_arc.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# ms=$1
+ms="ARC"
+
 DIR="${BASH_SOURCE%/*}"
 export $(grep -v '^#' "${DIR}/.env" | xargs)
 
-mkdir -p /mnt/ARC
-/sbin/mount.cifs -o user=$R_USER,pass=$R_PASS,file_mode=0644,dir_mode=0755,iocharset=utf8,vers=2.0 //"$R_HOST"/ARC /mnt/ARC
+mkdir -p /mnt/${ms}
+/sbin/mount.cifs -o user=$R_USER,pass=$R_PASS,file_mode=0644,dir_mode=0755,iocharset=utf8,vers=2.0 //"$R_HOST"/ARC /mnt/${ms}
